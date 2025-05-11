19fbfb5bb1c360656e664bee9bf359e78a0ba3f1
Roberto Castillo <defused15@gmail.com>
2025-05-11 14:50:38 -0600

    Edit changes

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,119 +1,73 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+ DivLogo,
+ InetumLogo,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
   name: "Roberto Castillo",
   initials: "RC",
-  location: "Torreón, Coahuila, México",
-  locationLink: "https://www.google.com/maps/place/torreon",
+  location: "México",
+  locationLink: "https://www.google.com/maps/place/mexico",
   about:
     "QA Engineer",
   summary:
-    "As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Experienced QA Engineer with a track record of success in over 10 projects across diverse industries, including e-commerce, logistics, and healthcare. Proficient in manual and automated testing, penetration testing (pentesting), and creating comprehensive test plans to ensure software reliability and security. Expertise includes designing and executing test cases, managing defects, and conducting a wide range of tests, including functional, integration, system, user acceptance, and performance testing. Skilled in using tools like Playwright, K6, Postman, Git/GitHub, and JIRA, with strong knowledge of HTML, CSS, JavaScript, TypeScript, and SQL. A collaborative professional adept at thriving in Agile environments, delivering secure, user-focused, and high-quality software solutions.",
+  avatarUrl: "https://github.com/Defused15.png",
+  personalWebsiteUrl: "",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "rtc18130984@gmail.com",
+    tel: "+528711053221",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/Defused15",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/rocastil/",
         icon: LinkedInIcon,
       },
-      {
+    /*  {
         name: "X",
         url: "https://x.com/BartoszJarocki",
         icon: XIcon,
-      },
+      },*/
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "University Instituto Tecnológico de La Laguna",
+      degree: "Bachelor of Mechatronic Engineering",
+      start: "2018",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
-      badges: ["Remote"],
-      title: "Software Architect",
-      logo: ConsultlyLogo,
-      start: "2024",
-      end: null,
-      description:
-        "Leading the development of the Film.io platform. Technologies: React, TypeScript, Node.js",
-    },
-    {
-      company: "Parabol",
+      company: "Inetum",
       link: "https://parabol.co",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led a squad, improved code delivery process, and initiated migration from Emotion to Tailwind CSS. Technologies: React, TypeScript, GraphQL",
+      title: "Quality Engineer",
+      logo: InetumLogo,
+      start: "2025",
+      end: null,
+      description: 
+      "In this role, I was responsible for reviewing and analyzing software requirements to ensure clarity, completeness, testability, and consistency. I created detailed test plans outlining the testing strategy, objectives, schedule, required resources, tools, and testing environments. Based on the software requirements and design documents, I designed and wrote test cases and scripts, including the creation of test data. I executed test cases across various stages such as functional, integration, system, and user acceptance testing. I also implemented automated testing scripts and frameworks to streamline and standardize testing cycles, integrating them with continuous integration and deployment pipelines. Defects identified during testing were thoroughly documented, tracked, and managed, with prioritization based on severity and impact. Regression testing was performed regularly to ensure new features or fixes did not introduce new issues. I assessed software performance in terms of speed, scalability, and stability under different conditions, and tested the product from an end-user perspective to validate usability and alignment with user expectations. Additionally, I reviewed related documentation for accuracy and consistency and collaborated closely with cross-functional teams to understand requirements and provide feedback on software quality throughout the development lifecycle.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
+      company: "Divelement",
+      link: "https://divelement.io/",
       badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, and AAA. Built a live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+      title: "QA Engineer",
+      logo: DivLogo,
+      start: "2023",
+      end: 2025,
+      description: 
+      "In this role, I was responsible for reviewing and analyzing software requirements to ensure clarity, completeness, testability, and consistency. I created detailed test plans outlining the testing strategy, objectives, schedule, required resources, tools, and testing environments. Based on the software requirements and design documents, I designed and wrote test cases and scripts, including the creation of test data. I executed test cases across various stages such as functional, integration, system, and user acceptance testing. I also implemented automated testing scripts and frameworks to streamline and standardize testing cycles, integrating them with continuous integration and deployment pipelines. Defects identified during testing were thoroughly documented, tracked, and managed, with prioritization based on severity and impact. Regression testing was performed regularly to ensure new features or fixes did not introduce new issues. I assessed software performance in terms of speed, scalability, and stability under different conditions, and tested the product from an end-user perspective to validate usability and alignment with user expectations. Additionally, I reviewed related documentation for accuracy and consistency and collaborated closely with cross-functional teams to understand requirements and provide feedback on software quality throughout the development lifecycle.",
     },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Led the Android team and developed apps for major Polish companies like LOT, Polskie Radio, Agora, and PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Developed and tested software for LTE base stations",
-    },
+
   ],
   skills: [
     "JavaScript",
@@ -124,6 +78,7 @@ export const RESUME_DATA = {
     "Relay",
     "WebRTC",
   ],
+  
   projects: [
     {
       title: "Parabol",
@@ -136,7 +91,7 @@ export const RESUME_DATA = {
       ],
       description:
         "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+      logo: DivLogo,
       link: {
         label: "github.com",
         href: "https://parabol.co/",
@@ -153,135 +108,13 @@ export const RESUME_DATA = {
       ],
       description:
         "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      logo: DivLogo,
       link: {
         label: "evercast.us",
         href: "https://www.evercast.us/",
       },
     },
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "Personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers, and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description: "Howdy is a place for joining communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+ 
   ],
+
 } as const;
